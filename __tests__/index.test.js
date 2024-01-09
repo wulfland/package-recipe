85ca5f60d99137b6012f40648871df7dabec0bbd
Michael Kaufmann <5276337+wulfland@users.noreply.github.com>
2024-01-09 11:27:44 +0100

    build: Add jest test and coverage badge
    Add a jest unit test and use make-coverage-badge to create a badge.

---
+++ __tests__/index.test.js
@@ -0,0 +1,13 @@
+/**
+ * Unit tests for the index.js
+ */
+
+describe('index.js', () => {
+it('console.log the text "Hello world!"', () => {
+    const indexMock = jest.spyOn(console, 'log');
+  
+    require('../src/index')
+  
+    expect(indexMock).toHaveBeenCalledWith('Hello world!');
+  })
+})
