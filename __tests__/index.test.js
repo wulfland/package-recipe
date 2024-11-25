632667c026fbfb5c6dcd1f99a81ab31ae4c51a63
Michael Kaufmann <5276337+wulfland@users.noreply.github.com>
2024-11-25 14:55:34 +0100

    breaking: Added a breaking change (#44)
    Another breaking change

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,6 +6,6 @@ describe('index.js', () => {
   it('greet function returns Hello world!', () => {
     const greet = require('../src/index')
 
-    expect(greet()).toBe('Hello world! v2.1.5')
+    expect(greet()).toBe('Hello world! v3.0.0')
   })
 })
