e1431cb76a1ec790b296fc71eec515faaac679ab
Michael Kaufmann <5276337+wulfland@users.noreply.github.com>
2024-11-26 16:55:49 +0100

    fix: Fix DDC demo 5
    Fixes: #47

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,6 +6,6 @@ describe('index.js', () => {
   it('greet function returns Hello world!', () => {
     const greet = require('../src/index')
 
-    expect(greet()).toBe('Hello world! #DDC24 4')
+    expect(greet()).toBe('Hello world! #DDC24 💩')
   })
 })
